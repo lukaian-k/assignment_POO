78546f86564a449e0d6a1d2aaddd8e1e9ce865d4
gatsby.anon <116958680+gatsby-anon@users.noreply.github.com>
2022-11-20 20:34:46 -0300

    Update order.cpp

--- src/objects/classificacao_cientifica/implementation/order.cpp
+++ src/objects/classificacao_cientifica/implementation/order.cpp
@@ -1,1 +1,17 @@
 #include "../order.h"
+
+string Order::get_order_name() {
+  return order_name == "" ? "Informe esta informação primeiro." : order_name;
+}
+
+string Order::get_order_description() {
+  return order_description == "" ? "Informe esta informação primeiro." : order_description;
+}
+
+bool Order::set_order_name(string order_name) { 
+  return set_string(this->order_name, order_name); 
+}
+
+bool Order::set_order_description(string order_description) {
+  return set_string(this->order_description, order_description);
+}

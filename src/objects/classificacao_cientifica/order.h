624e18825b39f8bb370d2ffef1f45f0b929dc4d5
gatsby.anon <116958680+gatsby-anon@users.noreply.github.com>
2022-11-20 20:29:21 -0300

    Update order.h

--- src/objects/classificacao_cientifica/order.h
+++ src/objects/classificacao_cientifica/order.h
@@ -2,5 +2,13 @@
 
 class Order : public Classe {
 private:
+  string order_name;
+  string order_description;
+
 public:
+  Order(string order_name, string order_description) : Classe (string class_name, string class_description);
+  string get_order_name();
+  string get_order_decription();
+  bool set_order_name(string order_name);
+  bool set_order_description(string order_description);
 };

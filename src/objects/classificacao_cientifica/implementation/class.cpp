efd69ff5f0aae3e2e345859491bb7ce1053a1dce
gatsby.anon <116958680+gatsby-anon@users.noreply.github.com>
2022-11-20 20:22:47 -0300

    Update class.cpp

--- src/objects/classificacao_cientifica/implementation/class.cpp
+++ src/objects/classificacao_cientifica/implementation/class.cpp
@@ -1,1 +1,17 @@
 #include "../class.h"
+
+string Classe::get_class_name() {
+  return class_name == "" ? "Informe esta informação primeiro." : class_name;
+}
+
+string Classe::get_class_description() {
+  return class_description == "" ? "Informe esta informação primeiro." : class_description;
+}
+
+bool Classe::set_class_name(string class_name) { 
+  return set_string(this->class_name, class_name); 
+}
+
+bool Classe::set_class_description(string class_description) {
+  return set_string(this->class_description, class_description);
+}

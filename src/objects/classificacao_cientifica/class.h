67824a80f7883fdafa51547d6ee6502f3192ee2e
gatsby.anon <116958680+gatsby-anon@users.noreply.github.com>
2022-11-20 19:48:35 -0300

    Update class.h

--- src/objects/classificacao_cientifica/class.h
+++ src/objects/classificacao_cientifica/class.h
@@ -2,5 +2,13 @@
 
 class Classe : public Phylum {
 private:
+  string class_name;
+  string description;
+  
 public:
+  Classe(string class_name) : Phylum (string name, string distinctive_characteristics);
+  string get_class_name();
+  string get_decription();
+  bool set_class_name(string class_name);
+  bool set_description(string description);
 };

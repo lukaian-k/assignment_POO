917e16d36789886a9cb0e81719ba746b0d21fad9
Lucas José <lukinhas8979@gmail.com>
2022-11-21 19:31:53 -0300

    iniciando o uso do cmake para compilar o projeto

--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 int main() {
   Animal a;
   cout << a.get_animal_name() << endl;
+  a.set_kingdom_name("animal");
+  cout << a.get_kingdom_name() << endl;
   return 0;
 }
--- src/objects/classificacao_cientifica/order.hpp
+++ src/objects/classificacao_cientifica/order.hpp
@@ -8,7 +8,7 @@ private:
 public:
   //Order(string order_name, string order_description) : Classe (string class_name, string class_description);
   string get_order_name();
-  string get_order_decription();
+  string get_order_description();
   bool set_order_name(string order_name);
   bool set_order_description(string order_description);
 };
